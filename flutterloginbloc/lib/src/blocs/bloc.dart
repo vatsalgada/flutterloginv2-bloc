b7ecca58a5afb0744ddd49cb08379bf4a27df023
vatsalgada <contactvgada@gmail.com>
2023-08-11 02:27:04 +0530

    addding transformer validations

--- flutterloginbloc/lib/src/blocs/bloc.dart
+++ flutterloginbloc/lib/src/blocs/bloc.dart
@@ -1,14 +1,21 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
+class Bloc with ValidationMixin {
   final _emailController = StreamController<String>();
   final _passwordController = StreamController<String>();
 
 //getters - Add data to stream (listen)
-  Stream<String> get email => _emailController.stream;
-  Stream<String> get password => _passwordController.stream;
+  Stream<String> get email => _emailController.stream.transform(validateEmail);
+  Stream<String> get password =>
+      _passwordController.stream.transform(validatePassword);
 
   //getters - Change data
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose() {
+    _emailController.close();
+    _passwordController.close();
+  }
 }
